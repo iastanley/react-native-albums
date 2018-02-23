8a28d5fb91d387c8018a5196be5f17de2687f6ad
iastanley <iasvks.craigslist@gmail.com>
2018-02-22 21:53:30 -0800

    added initial styles for the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 
-const Header = () => {
-  return <Text>Albums!</Text>;
-}
+const Header = ({ headerText }) => {
+  const { viewStyle, textStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{headerText}</Text>
+    </View>  
+  );
+};
+
+// all styles go in this styles object
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60, //what are the units here - pixels?!
+    elevation: 2,
+    position: 'relative',
+    ...Platform.select({
+      ios: {
+        // iOS only
+        paddingTop: 15,
+        shadowOpacity: 0.2,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 }, 
+      },
+      android: {
+        // Android only 
+        elevation: 2 // this is part of android material design support   
+      }
+    })
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
 
 export default Header;
+
+Header.defaultProps = {
+  headerText: 'Albums'
+};
